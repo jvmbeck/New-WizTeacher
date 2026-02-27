2d39595b87feefe441f6b26eb9f825f74a4e0a52
jvmbeck <jmadridbeck@gmail.com>
2026-02-27 12:16:46 -0300

    added date inputs to keep track of student's contract

--- src/components/admin-students/students/CreateStudentDialog.vue
+++ src/components/admin-students/students/CreateStudentDialog.vue
@@ -6,39 +6,100 @@
       </q-card-section>
 
       <q-form @submit="submitStudent">
-            <q-card-section class="q-gutter-md">
-              <q-input v-model="newStudent.name" label="Nome" outlined dense required />
-              <q-select
-                v-model="newStudent.book"
-                :options="bookOptions"
-                label="Livro"
-                outlined
-                dense
-                required
-                emit-value
-              />
-              <q-select
-                v-model="newStudent.currentLesson"
-                :options="lessonsOptions"
-                label="Lição Atual"
-                outlined
-                dense
-                required
-                :disable="!lessonsOptions.length"
-                emit-value
-              />
-              <q-select
-                v-model="selectedClassIds"
-                :options="classes"
-                option-label="name"
-                option-value="id"
-                emit-value
-                map-options
-                label="Selecionar Turmas"
-                multiple
-                outlined
-              />
-            </q-card-section>
+        <q-card-section class="q-gutter-md">
+          <q-input v-model="newStudent.name" label="Nome" outlined dense required />
+          <q-select
+            v-model="newStudent.book"
+            :options="bookOptions"
+            label="Livro"
+            outlined
+            dense
+            required
+            emit-value
+          />
+          <q-select
+            v-model="newStudent.currentLesson"
+            :options="lessonsOptions"
+            label="Lição Atual"
+            outlined
+            dense
+            required
+            :disable="!lessonsOptions.length"
+            emit-value
+          />
+          <q-select
+            v-model="selectedClassIds"
+            :options="classes"
+            option-label="name"
+            option-value="id"
+            emit-value
+            map-options
+            label="Selecionar Turmas"
+            multiple
+            outlined
+          />
+          <q-input v-model="newStudent.contract" label="Nº do contrato" outlined dense required />
+
+          <q-input
+            outlined
+            v-model="newStudent.bookStartDate"
+            mask="date"
+            :rules="['date']"
+            label="Começo do livro"
+          >
+            <template v-slot:append>
+              <q-icon name="event" class="cursor-pointer">
+                <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                  <q-date v-model="newStudent.bookStartDate">
+                    <div class="row items-center justify-end">
+                      <q-btn v-close-popup label="Close" color="primary" flat />
+                    </div>
+                  </q-date>
+                </q-popup-proxy>
+              </q-icon>
+            </template>
+          </q-input>
+
+          <q-input
+            v-if="isPartBBook(newStudent.book)"
+            outlined
+            v-model="newStudent.bookExchangeDate"
+            mask="date"
+            :rules="['date']"
+            label="Troca do livro"
+          >
+            <template v-slot:append>
+              <q-icon name="event" class="cursor-pointer">
+                <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                  <q-date v-model="newStudent.bookExchangeDate">
+                    <div class="row items-center justify-end">
+                      <q-btn v-close-popup label="Close" color="primary" flat />
+                    </div>
+                  </q-date>
+                </q-popup-proxy>
+              </q-icon>
+            </template>
+          </q-input>
+          <q-input
+            outlined
+            v-model="newStudent.bookEndDate"
+            mask="date"
+            :rules="['date']"
+            label="Término do Livro"
+          >
+            <template v-slot:append>
+              <q-icon name="event" class="cursor-pointer">
+                <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                  <q-date v-model="newStudent.bookEndDate">
+                    <div class="row items-center justify-end">
+                      <q-btn v-close-popup label="Close" color="primary" flat />
+                    </div>
+                  </q-date>
+                </q-popup-proxy>
+              </q-icon>
+            </template>
+          </q-input>
+        </q-card-section>
 
         <q-card-actions align="right">
           <q-btn flat label="Cancel" v-close-popup />
@@ -52,7 +113,6 @@
 <script setup>
 import { ref, watch, onMounted } from 'vue'
 import { useClassStore } from 'src/stores/classStore'
-import { useStudentStore } from 'src/stores/studentStore'
 import { storeToRefs } from 'pinia'
 
 const props = defineProps({
@@ -60,8 +120,6 @@ const props = defineProps({
 })
 
 const emit = defineEmits(['update:modelValue', 'create'])
-
-const studentStore = useStudentStore()
 
 const selectedClassIds = ref([])
 
@@ -77,31 +135,11 @@ const newStudent = ref({
   book: '',
   currentLesson: '1',
   classIds: [],
+  contract: '',
+  bookStartDate: '',
+  bookExchangeDate: '',
+  bookEndDate: '',
 })
-
-watch(
-  () => props.modelValue,
-  (val) => {
-    if (val) {
-      newStudent.value = {
-        name: '',
-        book: '',
-        currentLesson: '1',
-        classIds: [],
-      }
-      selectedClassIds.value = []
-    }
-  },
-)
-
-function emitClose(val) {
-  emit('update:modelValue', val)
-}
-import bookStructure from 'src/data/bookStructure.json'
-
-
-const bookOptions = Object.keys(bookStructure)
-const lessonsOptions = ref([])
 
 watch(
   () => props.modelValue,
@@ -112,12 +150,24 @@ watch(
         book: '',
         currentLesson: '',
         classIds: [],
+        contract: '',
+        bookStartDate: '',
+        bookExchangeDate: '',
+        bookEndDate: '',
       }
       selectedClassIds.value = []
       lessonsOptions.value = []
     }
   },
 )
+
+function emitClose(val) {
+  emit('update:modelValue', val)
+}
+import bookStructure from 'src/data/bookStructure.json'
+
+const bookOptions = Object.keys(bookStructure)
+const lessonsOptions = ref([])
 
 watch(
   () => newStudent.value.book,
@@ -132,6 +182,10 @@ watch(
   },
 )
 
+// check if the selected book is a Part B book (W2, T2, K2)
+function isPartBBook(book) {
+  return ['W2', 'T2', 'NEW K2', 'K2 3RD'].includes(book)
+}
 
 async function submitStudent() {
   newStudent.value.classIds = selectedClassIds.value
@@ -145,9 +199,7 @@ async function submitStudent() {
     return
   }
 
-  await studentStore.createStudent(newStudent.value)
   emit('create', { ...newStudent.value })
   emit('update:modelValue', false)
 }
-
 </script>
